dbe0192e5d7fc040235f40f5742d5f6cdbf81105
Krisu17 <01142550@pw.edu.pl>
2020-10-24 17:27:49 +0200

    app improved

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__, static_url_path="")
 def index():
     return render_template("index.html")
 
+@app.route("/register", methods=["GET"])
+def index():
+    return render_template("register.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
